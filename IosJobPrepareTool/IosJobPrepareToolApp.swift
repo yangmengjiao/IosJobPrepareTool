e6ccb74098fce77273190d7d636b881d343795ef
yangmengjiao <mengjiaoshun@gmail.com>
2021-01-28 11:41:38 -0600

    Initial Commit

---
+++ IosJobPrepareTool/IosJobPrepareToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosJobPrepareToolApp.swift
+//  IosJobPrepareTool
+//
+//  Created by mengjiao on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct IosJobPrepareToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
